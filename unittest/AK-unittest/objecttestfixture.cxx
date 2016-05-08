0fe9bcc4bbdfc07a32e37bb065a4d3ebea071b78
Matias Larsson <matias.larsson@gmail.com>
2016-05-08 23:09:49 +0300

    Added teardown to object unittest fixture

--- unittest/AK-unittest/objecttestfixture.cxx
+++ unittest/AK-unittest/objecttestfixture.cxx
@@ -1,15 +1,19 @@
-#include "objecttestfixture.hxx"
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <iostream>
 
+#include "objecttestfixture.hxx"
 #include "dbfactoryutil.hxx"
+#include "dbconfig.hxx"
 
 
 ObjectTestFixture::ObjectTestFixture()
     : m_db_factory(ak_util::getDBFactory())
 {
-    //ctor
+
 }
 
 ObjectTestFixture::~ObjectTestFixture()
 {
-    //dtor
+    SQLite::Database db(DBConfig::getInstance().filename(), SQLITE_OPEN_READWRITE);
+    db.exec("delete from object;");
 }
